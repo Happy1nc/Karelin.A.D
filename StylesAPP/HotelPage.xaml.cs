c94d8e158d64dda08917da14b34c107b30c1f11b
Happy1nc <89935987+Happy1nc@users.noreply.github.com>
2021-12-08 11:28:49 +0300

    Add files via upload

---
+++ StylesAPP/HotelPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace StylesAPP
+{
+    /// <summary>
+    /// Логика взаимодействия для HotelPage.xaml
+    /// </summary>
+    public partial class HotelPage : Page
+    {
+        public HotelPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AppEditPage());
+        }
+    }
+}
