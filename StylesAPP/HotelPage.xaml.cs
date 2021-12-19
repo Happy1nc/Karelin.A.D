44112501dec2862f8ba4ce49fbcfcad857671979
Happy1nc <89935987+Happy1nc@users.noreply.github.com>
2021-12-19 17:05:54 +0300

    Add files via upload
    AutorisationApp  of TourAgency

--- StylesAPP/HotelPage.xaml.cs
+++ StylesAPP/HotelPage.xaml.cs
@@ -21,6 +21,13 @@ namespace StylesAPP
     /// </summary>
     public partial class HotelPage : Page
     {
+        public string password;
+        public string login;
+        string loginAdmin = "Админ";
+        string passwordAdmin = "Админ";
+        string loginManager = "Менеджер";
+        string passwordManager = "Менеджер";
+
         public HotelPage()
         {
             InitializeComponent();
@@ -28,7 +35,39 @@ namespace StylesAPP
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            Manager.MainFrame.Navigate(new AppEditPage());
+            if (Loginbox.Text == loginAdmin && Passwordbox.Password == passwordAdmin)
+            {
+                password = Passwordbox.Password;
+                Manager.MainFrame.Navigate(new AdminPage());
+                MessageBox.Show("Добро пожаловать, администратор");
+            }
+
+            else if (Loginbox.Text == loginManager && Passwordbox.Password == passwordManager)
+            {
+                password = Passwordbox.Password;
+                Manager.MainFrame.Navigate(new ManagerPage());
+                MessageBox.Show("Добро пожаловать, менеджер");
+            }
+            else
+            {
+                MessageBox.Show("Неверные данные, повторите попытку", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void ShowPassword_Checked(object sender, RoutedEventArgs e)
+        {
+            if (ShowPassword.IsChecked == true)
+            {
+                Passwordbox.Visibility = Visibility.Hidden;
+                PasswordUnmask.Visibility = Visibility.Visible;
+                PasswordUnmask.Text = Passwordbox.Password;
+            }
+            else
+            {
+                Passwordbox.Visibility = Visibility.Visible;
+                PasswordUnmask.Visibility = Visibility.Hidden;
+                Passwordbox.Password = PasswordUnmask.Text;
+            }
         }
     }
 }
